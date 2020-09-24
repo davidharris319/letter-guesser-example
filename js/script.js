9d1d413d684827e22b10945a05906f67ce3e3a47
David Harris <davidharris319@gmail.com>
2020-09-23 22:51:41 -0400

    refactored code and added clickable buttons for the game

--- js/script.js
+++ js/script.js
@@ -25,75 +25,19 @@ const computerChoice = () => {
 //drying up our code
 const displayMessage = message => alert(message);
 
-// initialize game function
-const initializeGame = () => {
-    /* Flow
-    * 2 scenarios 
-    * A - on the first load
-    * B - after a win/loss game condition is met (aka after a game has finished)
-    *   - Show the wins, losses - increment of +1 to a loss/win based on game end
-    *   - Show the guesses left for a new game (same as the initial load) 
-    *   - Clean out the old guesses and show the user guesses array with no letters
-    */ 
-    
-    // Scenario A
+const takeTurn = userChoice => {
+    // decrement the number of guesses and show the letter guessed
+    guessesLeft -= 1;
+    userGuesses.push(userChoice);
 
-    // Check if the user has already played the game and is resetting
-    if (userGuesses.length > 0 && guessesLeft !== 10) {
-        userGuesses = []; // when we reset/re-initialize the game, update hte previous game's guesses back to nothing
-        guessesLeft = 10;
-    }
-
-    winsElement();
-    lossesElement();
-    guessesLeftElement();
+    // Show the letters guessed
     userGuessesElement();
-    computerChoice();
+    guessesLeftElement();
+}
 
-};
-
-// UI Functions --> use functions to store code that will manipulate the DOM for you.
-    //set the value of the span with ID win in our front end to the value of the UI variable at the time we call the function
-const winsElement = () => (document.getElementById('wins').innerHTML = wins);
-const lossesElement = () => (document.getElementById('losses').innerHTML = losses);
-const guessesLeftElement = () => (document.getElementById('guesses-left').innerHTML = guessesLeft);
-const userGuessesElement = () => (document.getElementById('user-guesses').innerHTML = userGuesses);
-
-// Event Listeners
-
-document.addEventListener('keypress', function(event) {
-    // save the value of the key that was pressed into a variable
-
-    /* only lower case
-
-   
-    */
-    const userChoice = event.key.toLowerCase();
-
-    // excludes numeric
-    // excludes special characters
-    if (!letters.includes(userChoice)) {
-        // Show an error message
-        displayMessage('No special characters or numbers, please pick a letter from the alphabet');
-
-    // exclude duplicate choices
-    } else if (userGuesses.includes(userChoice)) {
-        displayMessage('Sorry, but you cannot choose the same letter twice');
-    } else {
-        // decrement the number of guesses and show the letter guessed
-        guessesLeft -= 1;
-        userGuesses.push(userChoice);
-
-        // Show the letters guessed
-        userGuessesElement();
-        guessesLeftElement();
-    }
- 
-
-    console.log('guesses left', guessesLeft);
-    console.log(userChoice)
-    // check the userChoice against the letter chosen by the PC
-    if(randomLetter === userChoice) {
+const checkWinCondition = userChoice => {
+       // check the userChoice against the letter chosen by the PC
+       if(randomLetter === userChoice) {
         // if right --> win, then increment wins and show winning percentage
         displayMessage("Way to go!!! You're a freaking mind reader!!!")
         wins += 1;
@@ -106,12 +50,80 @@ document.addEventListener('keypress', function(event) {
         losses += 1;
         initializeGame();
     }
+}
 
+const checkForValidTurn = userChoice => {
+    // excludes numeric
+    // excludes special characters
+    if (!letters.includes(userChoice)) {
+        // Show an error message
+        displayMessage('No special characters or numbers, please pick a letter from the alphabet');
+    // exclude duplicate choices
+    } else if (userGuesses.includes(userChoice)) {
+        displayMessage('Sorry, but you cannot choose the same letter twice');
+    } else {
+        takeTurn(userChoice);
+    }
+}
+
+
+const renderLetters = () => {
+    let letterButtons = '';
+
+    letters.forEach(function(letter) {
+        letterButtons += `<button type="button" class="btn btn-outline-info" style="margin:3px">${letter}</button>`
+    });
+
+    return letterButtons;
+}
+
+// initialize game function
+const initializeGame = () => {
+    /* Flow
+    * 2 scenarios 
+    * A - on the first load
+    * B - after a win/loss game condition is met (aka after a game has finished)
+    *   - Show the wins, losses - increment of +1 to a loss/win based on game end
+    *   - Show the guesses left for a new game (same as the initial load) 
+    *   - Clean out the old guesses and show the user guesses array with no letters
+    */ 
+
+    // Check if the user has already played the game and is resetting
+    if (userGuesses.length > 0 && guessesLeft !== 10) {
+        userGuesses = []; // when we reset/re-initialize the game, update hte previous game's guesses back to nothing
+        guessesLeft = 10;
+    }
+
+    winsElement();
+    lossesElement();
+    guessesLeftElement();
+    userGuessesElement();
+    computerChoice();
+    lettersContainerElement(renderLetters());
+};
+
+// UI Functions --> use functions to store code that will manipulate the DOM for you.
+    //set the value of the span with ID win in our front end to the value of the UI variable at the time we call the function
+const winsElement = () => (document.getElementById('wins').innerHTML = wins);
+const lossesElement = () => (document.getElementById('losses').innerHTML = losses);
+const guessesLeftElement = () => (document.getElementById('guesses-left').innerHTML = guessesLeft);
+const userGuessesElement = () => (document.getElementById('user-guesses').innerHTML = userGuesses.join(', '));
+
+const lettersContainerElement = string => (document.getElementById('letter-buttons-container').innerHTML = string);
+
+// Event Listeners
+
+document.addEventListener('keypress', function(event) {
+    const userChoice = event.key.toLowerCase();
+    checkForValidTurn(userChoice);
+    checkWinCondition(userChoice);
+});
+
+document.querySelector('#letter-buttons-container').addEventListener('click', function(event) {
+    const userChoice = event.target.innerHTML;
+    checkForValidTurn(userChoice);
+    checkWinCondition(userChoice);
 })
-    // Listen for the user to type a key in the keyboard
-    // If win, then increment wins and show winning message
-    // If Wrong, then add decrement number of guesses, check that number of guesses not 0 and show the letters guessed
-
 
 // Initialize the application
 initializeGame();
